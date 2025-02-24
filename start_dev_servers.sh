e18ac8db1fa37b2a81e066c8594906033081dfe2
Muhammad Osman <139002050+MuhammadOsman381@users.noreply.github.com>
2025-02-24 09:55:08 +0500

    Update start_dev_servers.sh

--- start_dev_servers.sh
+++ start_dev_servers.sh
@@ -9,26 +9,6 @@ startServer() {
 startClient &
 startServer
 wait
-
-
-
-AIzaSyA5URFbJlN9qFZMgzVhWOlnlgMzlJ7nsS8
-
-
-
-if (text.startsWith("```") && text.endsWith("```")) {
-      const code = text.slice(3, -3).trim();
-      return (
-        <SyntaxHighlighter language="javascript" style={tomorrow}>
-          {code}
-        </SyntaxHighlighter>
-      );
-    }
-    
-    
-    
-    
-    
     
     
     
